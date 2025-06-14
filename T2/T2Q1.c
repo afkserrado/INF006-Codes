9a73b1764e4d2937d9aa4befb57ee8d504e5fc52
afkserrado <akserrado@gmail.com>
2025-06-13 22:49:04 -0300

    Correção da separação dos tokens e inclusão na lista

---
+++ T2/T2Q1.c
@@ -0,0 +1,14 @@
+/*
+Instituto Federal da Bahia (IFBA)
+Tecnólogo em Análise e Desenvolvimento de Sistemas (ADS)
+Semestre 2025.1
+INF006 - Estrutura de Dados e Algoritmos
+Professor: José Dihego
+Aluno: Anderson Serrado
+T2Q2
+
+Implemente uma pilha de texto todos os elementos estão sempre ordenados alfabeticamente sem ferir a politica LIFO (las-in first-out).
+
+Obs. 1: entende-se que os nomes são todos simples.
+Obs. 2: entende-se que os nomes começam com uma letra capital.
+*/
