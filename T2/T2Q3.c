8c52a4d6c3b939a054e15e82db51cdfae74df58b
afkserrado <akserrado@gmail.com>
2025-06-16 01:22:59 -0300

    Implantação da lista LI

--- T2/T2Q3.c
+++ T2/T2Q3.c
@@ -22,7 +22,7 @@ T2Q3
 #define dimLinha 3001 // 3000 caracteres + 1 para o \0
 
 // ##################################################### //
-// LISTA DUPLAMENTE ENCADEADA
+// LISTA DUPLAMENTE ENCADEADA LINEAR: LE (INTEIROS)
 
 // Define a estrutura de um nó da lista de inteiros (LE)
 typedef struct iNode {
@@ -118,6 +118,99 @@ void liberar_iLista(iLista *lista) {
 }
 
 // ##################################################### //
+// LISTA SIMPLESMENTE LIGADA CIRCULAR: LI (REAIS)
+
+// Define a estrutura de um nó da lista de reais (LI)
+typedef struct rNode {
+    float chave;
+    struct rNode *prox;
+} rNode;
+
+// Define a estrutura da lista de reais
+typedef struct rLista {
+    rNode *cabeca;
+    rNode *cauda;
+} rLista;
+
+// Cria e inicializa um novo nó na lista de reais, configurando seus ponteiros para NULL
+rNode *init_rNode (float chave) {
+    rNode *node_novo = malloc(sizeof(rNode)); // Aloca memória para o primeiro nó
+    node_novo->chave = chave;
+    node_novo->prox = NULL;
+    return node_novo;
+}
+
+// Cria e inicializa a lista de reais, configurando o head para NULL (lista vazia)
+rLista *init_rLista () {
+    rLista *LI = malloc(sizeof(rLista));
+    LI->cabeca = NULL;
+    LI->cauda = NULL;
+    return LI;
+}
+
+// Insere um novo nó na lista de reais, na posição ordenada (ordem decrescente)
+void inserir_rNode_ordenado (rLista *LI, rNode *node_novo) {
+    // Lista vazia
+    if (LI->cabeca == NULL) {
+        LI->cabeca = node_novo;
+        LI->cauda = node_novo;
+        node_novo->prox = LI->cabeca; // O novo nó aponta para ele mesmo, fechando o círculo
+    }
+    // Lista não vazia
+    else {
+        
+        // Variáveis temporárias
+        rNode *x = LI->cabeca; // Marca a chave imediatamente posterior o node_novo
+        rNode *y = NULL; // Marca a chave imediatamente anterior ao node_novo
+        float chave = node_novo->chave;
+
+        // Encontra a posição correta
+        while (x != NULL && x->chave > chave) {
+            y = x;
+            x = x->prox;
+        }
+
+        // Caso 1: o node_novo é o primeiro item da lista
+        if (x == LI->cabeca) {
+            node_novo->prox = LI->cabeca;
+            LI->cabeca = node_novo;
+            LI->cauda->prox = LI->cabeca; // Fecha o círculo
+        }
+        // Caso 2: o node_novo é o último item da lista
+        else if (x == NULL) {
+            node_novo->prox = LI->cabeca;
+            LI->cauda->prox = node_novo;
+            LI->cauda = node_novo;
+        }
+        // Caso 3: o node_novo ocupa qualquer posição intermediária da lista
+        else {
+            node_novo->prox = x;
+            y->prox = node_novo;     
+        }
+    }
+}
+
+void imprimir_rLista (rLista *LI, FILE *arqSaida) {
+    rNode *x = LI->cabeca; // Inicializa x com a "cabeca" da lista
+    fprintf(arqSaida, "LI");
+    do {
+        fprintf(arqSaida, " %.2f", x->chave);     
+        x = x->prox;
+    } while (x != LI->cabeca);
+}
+
+// Função para liberar todos os nós da lista principal
+void liberar_rLista(rLista *LI) {
+    rNode *x = LI->cabeca;
+    do {
+        rNode *temp = x;
+        x = x->prox;
+        free(temp); // Libera a memória de cada nó
+    } while (x != LI->cabeca);
+    free(LI); // Libera a memória da lista
+}
+
+// ##################################################### //
 // MAIN
 
 int main () {
@@ -145,9 +238,10 @@ int main () {
 
         // Inicialização das listas
         iLista *LE = init_iLista(); // LE = lista externa (inteiros)
+        rLista *LI = init_rLista(); // LI = lista interna (reais)
 
         // Verificar se a alocação de memória falhou
-        if (LE == NULL) {
+        if (LE == NULL || LI == NULL) {
             fprintf(arqSaida, "Erro ao alocar memória para as listas. Pulando para a próxima linha.\n");
             continue; // Passa para a próxima linha sem parar o programa
         }
@@ -160,7 +254,10 @@ int main () {
         // Limpa a entrada
         entrada[strcspn(entrada, "\n")] = '\0'; // Remove o \n (caso exista)
         char *linha = entrada + 3; // Pula o "LE "
-                
+        
+        // ##################################################### //
+        // LE (INTEIROS)
+
         // Pega a lista LE (inteiros)
         token = strtok_r(linha, del1, &saveptr1); // Até encontrar "LI"
 
@@ -185,25 +282,32 @@ int main () {
             subtoken = strtok_r(NULL, del2, &saveptr2);
         }
 
-        // Pega a lista LI (decimais)
-        token = strtok_r(NULL, del1, &saveptr1); // Pega o restante após "LI"
-        printf("Decimais = %s\n", token);
+        // ##################################################### //
+        // LI (REAIS)
 
-        // Verifica se a lista LI ou o subtoken estão vazios
+        // Pega a lista LI (reais)
+        token = strtok_r(NULL, del1, &saveptr1); // Pega o restante após "LI"
+        printf("decimais = %s\n", token);
+
+        // Verifica se a lista LI está vazia
         if (token == NULL) {  // Caso a LI esteja vazia ou com formato incorreto
-            //fprintf(arqSaida, "Erro: linha vazia ou inválida.");
+            fprintf(arqSaida, "Erro: linha vazia ou inválida.");
             flag = 1;
             continue; // Pula para a próxima linha
         }
         
-        // Pega o primeiro subtoken da lista LE
+        // Pega o primeiro subtoken (real) da lista LI
         subtoken = strtok_r(token, del2, &saveptr2);
 
-        // Pega os demais subtokens (decimais) da lista LI
-        while (subtoken != NULL) {
-            // Inicializa as variáveis auxiliares
+        // Pega os demais subtokens (reais) da lista LI
+        while (subtoken != NULL) {       
+            // Insere um novo nó na lista de reais
             float num = atof(subtoken);
-            printf("Subtoken = %.2f\n", num);
+            printf("decimal = %.2f\n", num);
+            rNode *temp = init_rNode(num);
+            inserir_rNode_ordenado(LI, temp);
+
+            // Pega o próximo subtoken (float) da lista LI
             subtoken = strtok_r(NULL, del2, &saveptr2);
         }
         
@@ -211,13 +315,15 @@ int main () {
         flag = 1;
 
         imprimir_iLista(LE, arqSaida);
+        fprintf(arqSaida, " ");
+        imprimir_rLista(LI, arqSaida);
 
         // Libera a memória alocada para as listas após cada linha
         liberar_iLista(LE);
-        //liberar_sLista(LI);
+        liberar_rLista(LI);
     }
 
     fclose(arqEntrada); // Fecha o arquivo e libera a memória
-    //fclose(arqSaida); // Fecha o arquivo e libera a memória
+    fclose(arqSaida); // Fecha o arquivo e libera a memória
     return EXIT_SUCCESS;
 }
